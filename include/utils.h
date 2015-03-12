075dbd286ffcd36682e3a617cd8c408f886efac7
guillermohbm <guillermohbm@gmail.com>
2015-03-12 09:51:18 -0300

    fix bug gcc compiler

--- include/utils.h
+++ include/utils.h
@@ -12,9 +12,9 @@ void removeRow(Eigen::MatrixXd& matrix, unsigned int rowToRemove);
 void flat(Eigen::MatrixXd& mat);
 Eigen::VectorXd average(Eigen::MatrixXd a,Eigen::MatrixXd weigths, int axis);
 double median(Eigen::MatrixXd med);
-static inline float fastlog2 (float x);
-static inline float fastlog (float x);
-static inline float fastdigamma (float x);
+float fastlog2 (float x);
+float fastlog (float x);
+float fastdigamma (float x);
 Eigen::MatrixXd psi(Eigen::MatrixXd mat);
 float psi(float x);
 

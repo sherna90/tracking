00b7e63c687193daaf7654488f7462250ba397fe
nicolás fuentes <nicofuentes47.com>
2015-04-30 12:52:32 -0300

    multinomial multinomialnaivebayes cpp

--- src/multinomial.cpp
+++ src/multinomial.cpp
@@ -14,34 +14,27 @@ Multinomial::Multinomial(MatrixXd &counts, double alpha)
 {
     double total=counts.sum()+counts.cols()*alpha;
     theta=VectorXd(counts.cols());
-    for (int i = 0; i < counts.cols(); ++i) {
-        theta(i)=(counts.col(i).sum()+alpha)/total;
-    }
+        for (long i = 0; i < counts.cols(); ++i) {
+            theta(i)=(counts.col(i).sum()+alpha)/total;
+            //std::cout <<"Tct:"<<counts.col(i).sum()<<std::endl;
+        }
 }
 
 double Multinomial::log_likelihood(VectorXd test)
 {
     double log_like=0.0;
-    VectorXd log_test=VectorXd::Zero(test.size());
-    VectorXd log_theta=VectorXd::Zero(test.size());
-
-    for(int i=0;i<test.size();i++){
-            log_test[i]=lgamma(test[i]+1);
-            log_theta[i]=test[i]*log(this->theta[i]);
+    double sum_test=0.0;
+    double sum_theta=0.0;
+    long i=0;
+#pragma omp parallel for private(i) reduction(+:sum_test) reduction(+:sum_theta)
+    for(i=0;i<test.size();i++){
+        sum_test+=lgamma(test[i]+1);
+        sum_theta+=test[i]*log(this->theta[i]);
     }
-    log_like=lgamma(test.sum()+1)-log_test.sum()+log_theta.sum();
+    log_like=lgamma(test.sum()+1)-sum_test+sum_theta;
     return log_like;
 }
 
-VectorXd Multinomial::getTct() const
-{
-    return Tct;
-}
-
-void Multinomial::setTct(const VectorXd &value)
-{
-    Tct = value;
-}
 VectorXd Multinomial::getTheta() const
 {
     return theta;
--- src/multinomialnaivebayes.cpp
+++ src/multinomialnaivebayes.cpp
@@ -11,15 +11,14 @@ MultinomialNaiveBayes::MultinomialNaiveBayes(MatrixXd &datos,VectorXi &clases)
     Y=&clases;
     initialized=get_classes();
 }
-
 void MultinomialNaiveBayes::fit()
 {
     if(initialized){
-        std::map<int,std::vector<int> >::iterator iter;
+        std::map<long,std::vector<long>>::iterator iter;
         for (iter = Xc.begin(); iter != Xc.end(); ++iter) {
-            int nc=iter->second.size();
+            long nc=iter->second.size();
             MatrixXd Xtr(nc,getX()->cols());
-            for (int i = 0; i < nc; ++i) {
+            for (long i = 0; i < nc; ++i) {
                 Xtr.row(i)=getX()->row(iter->second[i]);
             }
             classes[iter->first]=Multinomial(Xtr,1.0);
@@ -30,19 +29,21 @@ void MultinomialNaiveBayes::fit()
 VectorXi MultinomialNaiveBayes::test(MatrixXd &Xtest)
 {
     VectorXi c=VectorXi::Zero(Xtest.rows());
-    //std::ofstream likelihood_txt("likelihood.txt");
-    // original...
-
+    double max_class=0.0;
+    double max_score=-100000000.0;
+    double score=0;
+    std::map<long,Multinomial>::iterator iter;
+#pragma omp parallel for private(max_class,max_score,score,iter)
     for (long i = 0; i < Xtest.rows(); ++i) {
-        std::map<int,Multinomial>::iterator iter;
-        double max_class=0.0;
-        double max_score=-100000000.0;
+        max_class=0.0;
+        max_score=-100000000.0;
         for (iter = classes.begin(); iter != classes.end(); ++iter) {
-            double score=log(getPrior()[iter->first])+iter->second.log_likelihood(std::move(Xtest.row(i)));
+           score=log(getPrior()[iter->first])+iter->second.log_likelihood(std::move(Xtest.row(i)));
             if(score > max_score){
                 max_score=score;
                 max_class=iter->first;
             }
+            score=0;
         }
         c(i)=max_class;
     }
@@ -55,7 +56,8 @@ bool MultinomialNaiveBayes::get_classes()
         Xc[(*getY())(i)].push_back(i);
     }
 
-    std::map<int,std::vector<int> >::iterator iter;
+    std::map<long,std::vector<long>>::iterator iter;
+
     for (iter = Xc.begin(); iter != Xc.end(); ++iter) {
         Prior[iter->first]=(double)getXc()[iter->first].size()/getX()->rows();
     }
@@ -79,21 +81,21 @@ void MultinomialNaiveBayes::setY(const VectorXi *value)
 {
     Y = value;
 }
-std::map<int, std::vector<int> > MultinomialNaiveBayes::getXc() const
+std::map<long, std::vector<long> > MultinomialNaiveBayes::getXc() const
 {
     return Xc;
 }
 
-void MultinomialNaiveBayes::setXc(const std::map<int, std::vector<int> > &value)
+void MultinomialNaiveBayes::setXc(const std::map<long, std::vector<long> > &value)
 {
     Xc = value;
 }
-std::map<int, double> MultinomialNaiveBayes::getPrior() const
+std::map<long, double> MultinomialNaiveBayes::getPrior() const
 {
     return Prior;
 }
 
-void MultinomialNaiveBayes::setPrior(const std::map<int, double> &value)
+void MultinomialNaiveBayes::setPrior(const std::map<long, double> &value)
 {
     Prior = value;
 }

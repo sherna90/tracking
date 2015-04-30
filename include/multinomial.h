978dc256369d2249fc01f327e8de2055e6658359
nicolás fuentes <nicofuentes47.com>
2015-04-30 12:49:46 -0300

    multinomia

--- include/multinomial.h
+++ include/multinomial.h
@@ -1,12 +1,11 @@
 #ifndef MULTINOMIAL_H
 #define MULTINOMIAL_H
 
-#include <float.h>
-#include <vector>
-#include <map>
-#include <iostream>
+#include <stdlib.h>
 #include <cmath>
-#include <Eigen/Dense>
+#include "../Eigen/Core"
+#include "../Eigen/Dense"
+#include <iostream>
 
 using namespace Eigen;
 
@@ -17,15 +16,13 @@ public:
     Multinomial(MatrixXd &counts);
     Multinomial(MatrixXd &counts, double alpha);
     double log_likelihood(VectorXd test);
-    VectorXd getTct() const;
-    void setTct(const VectorXd &value);
 
     VectorXd getTheta() const;
     void setTheta(const VectorXd &value);
+
+
 private:
     VectorXd theta;
-    VectorXd Tct;
-
 };
 
 #endif // MULTINOMIAL_H

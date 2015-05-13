b70fdbeb20e7863b7fd53ed784da5afc9c200a12
guillermohbm <guillermohbm@gmail.com>
2015-05-13 11:30:34 -0300

    add gaussian class

---
+++ include/gaussian.hpp
@@ -0,0 +1,28 @@
+#ifndef GAUSSIAN_H
+#define GAUSSIAN_H
+
+#include <cmath>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+
+class Gaussian
+{
+    public:
+        Gaussian(){mean=0;sd=0;};
+        Gaussian(VectorXd& data);
+        //double log_likelihood(VectorXd test);
+        double log_likelihood(double test);
+        //double likelihood(VectorXd test);
+        double likelihood(double test);
+        double getMean();
+        void setMean( double n);
+        void setSd( double n);
+    private:
+        double mean,sd;
+};
+
+
+
+#endif 

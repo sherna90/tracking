a72a5c23745a6857795c9b7c4faf993b41f4336e
dvergara <reiby-viper@hotmail.com>
2016-11-09 12:49:25 -0300

    fix Hamiltonian Monte Carlo

--- src/likelihood/hamiltonian_monte_carlo.hpp
+++ src/likelihood/hamiltonian_monte_carlo.hpp
@@ -20,10 +20,10 @@ class Hamiltonian_MC
 public:
 	Hamiltonian_MC();
 	Hamiltonian_MC(MatrixXd &_X,VectorXd &_Y, double _lamda);
-	VectorXd run(int _iterations, double step_size, int num_step);
-	VectorXd simulation(VectorXd &_initial_x, double _step_size, int _num_step);
+	VectorXd run(int _iterations, double _step_size, int _num_step);
+	VectorXd simulation(VectorXd &_initial_x);
 private:
-	void leap_Frog(VectorXd &_x0, VectorXd &_v0, VectorXd &x, VectorXd &v, double _step_size, int _num_step);
+	void leap_Frog(VectorXd &_x0, VectorXd &_v0, VectorXd &x, VectorXd &v);
 	double hamiltonian(VectorXd &_position, VectorXd &_velocity);
 	double kinetic_energy(VectorXd &_velocity);
 	bool init;
